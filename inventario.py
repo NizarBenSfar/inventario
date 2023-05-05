fc1ade3898a664b07d21961cd4aedbdac13184b7
NizarBenSfar <bensfarnizar92@gmail.com>
2023-05-05 16:03:22 +0200

    typo

--- inventario.py
+++ inventario.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-
+from datetime import datetime
+import numpy as np
 def read_excel(name = "db/Invetario.xlsx"):
     df = pd.read_excel(name) 
     return df
@@ -9,7 +10,7 @@ def read_excel(name = "db/Invetario.xlsx"):
 st.title("INVENTARIO")
 
 df = read_excel()
-edited_df = st.experimental_data_editor(df,um_rows="dynamic")
+edited_df = st.experimental_data_editor(df,num_rows="dynamic")
 
 with st.button("SAVE"):
-    df.to_excel("db/Invetario.xlsx")
+    edited_df.to_excel("db/Invetario.xlsx")
