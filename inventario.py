33e8e21c9d5d1a8535c20c201598c56877b9d1b3
NizarBenSfar <bensfarnizar92@gmail.com>
2023-05-05 15:44:16 +0200

    f

--- inventario.py
+++ inventario.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-def read_excel(name = "db\Invetario.xlsx"):
+def read_excel(name = "db/Invetario.xlsx"):
     df = pd.read_excel(name) 
     return df
 
