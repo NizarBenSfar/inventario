5b3fa4dca78ab33031c64e79e957bf275696e5a2
NizarBenSfar <bensfarnizar92@gmail.com>
2023-05-05 15:40:31 +0200

    read_df

--- inventario.py
+++ inventario.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+def read_excel(name = "db\Invetario.xlsx"){
+    df = pd.read_excel(name) 
+    return df
+}
 
 st.title("INVENTARIO")
 st.write("INVENTARIO")
+
+df = read_excel()
+st.dataframe(df)
