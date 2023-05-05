7977289506f5766bc6c030022bf74a73d168d076
NizarBenSfar <bensfarnizar92@gmail.com>
2023-05-05 15:41:22 +0200

    read commit typo

--- inventario.py
+++ inventario.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-def read_excel(name = "db\Invetario.xlsx"){
+def read_excel(name = "db\Invetario.xlsx"):
     df = pd.read_excel(name) 
     return df
-}
+
 
 st.title("INVENTARIO")
 st.write("INVENTARIO")
